94eb2a5b2f302eb87469d5fb88bc0666904184e4
Michael Rawlings <ml.rawlings@gmail.com>
2017-01-07 10:37:03 -0800

    add support for running production

--- src/start.js
+++ src/start.js
@@ -1,6 +1,14 @@
-require('browser-refresh').start({
-    script: require.resolve('../app/index.js'),
-    delay: 3000,
-    execArgs: [],
-    args: []
-});
+var NODE_ENV = process.env.NODE_ENV;
+var isDev = NODE_ENV == null ||
+            NODE_ENV.toLowerCase() === 'development'.slice(0, NODE_ENV.length);
+
+if(isDev) {
+    require('browser-refresh').start({
+        script: require.resolve('../app/index.js'),
+        delay: 3000,
+        execArgs: [],
+        args: []
+    });
+} else {
+    require('../app/index.js');
+}
