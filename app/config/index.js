11cc6606e62b0cafbb015f7d346f42100c19cd4c
Michael Rawlings <ml.rawlings@gmail.com>
2017-01-12 12:26:57 -0800

    remove babel and app-module-path

--- app/config/index.js
+++ app/config/index.js
@@ -12,6 +12,7 @@ var isProd = !isDev;
 exports.isDev = isDev;
 exports.isProd = isProd;
 
+
 /*
 Allow requiring paths relative to the root of the project:
 require('~/src');
@@ -19,38 +20,23 @@ require('~/src');
 require('require-self-ref');
 
 /*
-Allow directories under lib to be used as if they were in node_modules
-*/
-var libDir = path.join(projectDir, 'lib');
-var appModulePath = require('app-module-path');
-appModulePath.addPath(libDir);
-
-/*
 Allow requiring *.marko files
 */
 require('marko/node-require').install();
 require('marko/compiler/config').meta = true;
+
 
 /*
 Enable res.marko
 */
 require('marko/express');
 
+
 /*
 Browser Refresh
 */
 require('marko/browser-refresh').enable();
 require('lasso/browser-refresh').enable('*.marko *.css *.less *.styl *.scss *.sass *.png *.jpeg *.jpg *.gif *.webp *.svg');
-
-/*
-ES Modules
-*/
-require('babel-register')({
-    ignore: /node_modules/,
-    plugins: [
-        require('babel-plugin-transform-es2015-modules-commonjs'),
-    ],
-});
 
 /*
 Lasso
